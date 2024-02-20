acd096bbae1fb35d464177e885f7e5e4e7fd9113
eaglercraft <noreply@git.eaglercraft.rip>
2024-02-19 16:33:50 -0800

    Fix Desktop Runtimes for Linux

--- Eaglercraft_b1.3_Desktop_Runtime/_start_desktop_runtime.bat
+++ Eaglercraft_b1.3_Desktop_Runtime/_start_desktop_runtime.bat
@@ -1,4 +1,4 @@
 @echo off
 title Eaglercraft Beta 1.3 Desktop Runtime
-java -Xmx1G -Xms1G -Dsun.java2d.uiScale=1 -Djava.library.path=. -cp ./* net.lax1dude.eaglercraft.MinecraftMain
+java -Xmx1G -Xms1G -Dsun.java2d.uiScale=1 -Djava.library.path=. -cp "eaglercraft.jar;lwjgl.jar;lwjgl_util.jar;Java-WebSocket-1.5.1-with-dependencies.jar" net.lax1dude.eaglercraft.MinecraftMain
 pause
--- Eaglercraft_b1.3_Desktop_Runtime/_start_desktop_runtime.sh
+++ Eaglercraft_b1.3_Desktop_Runtime/_start_desktop_runtime.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Xmx1G -Xms1G -Dsun.java2d.uiScale=1 -Djava.library.path=. -cp ./* net.lax1dude.eaglercraft.MinecraftMain
+java -Xmx1G -Xms1G -Dsun.java2d.uiScale=1 -Djava.library.path=. -cp "eaglercraft.jar:lwjgl.jar:lwjgl_util.jar:Java-WebSocket-1.5.1-with-dependencies.jar" net.lax1dude.eaglercraft.MinecraftMain
